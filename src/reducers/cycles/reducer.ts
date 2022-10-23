afb3a4d7febc7646531dc8b829b4c4ea79623887
jose-luiz <jose.iscosistemas@gmail.com>
2022-10-23 13:06:10 -0700

    Immer

--- src/reducers/cycles/reducer.ts
+++ src/reducers/cycles/reducer.ts
@@ -32,8 +32,34 @@ export function cyclesReducers(state: CyclesState, action: any) {
         draft.activeCycle = null;
         draft.cycles[currentIndex].interruptDate = new Date();
       });
+      // return {
+      //   ...state,
+      //   cycles: state.cycles.map((cycle) => {
+      //     if (cycle.id === state.activeCycle?.id) {
+      //       return {
+      //         ...cycle,
+      //         interruptDate: new Date(),
+      //       };
+      //     }
+      //     return cycle;
+      //   }),
+      //   activeCycle: null,
+      // };
     }
     case CyclesActionTypes.MARK_CURRENT_CYCLE_AS_DONE: {
+      // return {
+      //   ...state,
+      //   cycles: state.cycles.map((c) => {
+      //     if (c.id === state.activeCycle?.id) {
+      //       return {
+      //         ...c,
+      //         endDate: new Date(),
+      //       };
+      //     }
+      //     return c;
+      //   }),
+      //   activeCycle: null,
+      // };
       const currentIndex = state.cycles.findIndex((cycle) => cycle.id === state.activeCycle?.id);
 
       if (currentIndex < 0) {
